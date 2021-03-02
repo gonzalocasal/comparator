89948e7070f04a0a32f35c27c0e83cba292e545b
gonzalo <gonzalocasal@gmail.com>
2021-03-02 20:14:31 -0300

    externalize messages

--- src/main/java/com/comparator/versionchecker/rest/VersionsController.java
+++ src/main/java/com/comparator/versionchecker/rest/VersionsController.java
@@ -4,6 +4,7 @@ import com.comparator.versionchecker.model.Conclusion;
 import com.comparator.versionchecker.service.InputSanitizer;
 import com.comparator.versionchecker.model.VersionComparator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("versions/compare")
+
 public class VersionsController {
 
   private final InputSanitizer inputSanitizer;
   private final VersionComparator versionComparator;
+
+  @Value("${template.versions.compare.response}")
+  private String responseTemplate;
 
   @Autowired
   public VersionsController(InputSanitizer inputSanitizer, VersionComparator versionComparator) {
@@ -26,10 +31,10 @@ public class VersionsController {
 
   @GetMapping("/{inputA}/with/{inputB}")
   public String compare(@PathVariable String inputA, @PathVariable String inputB) {
-
     log.info("Checking the versions {} and {}", inputA, inputB);
     Conclusion conclusion = versionComparator.compare(inputSanitizer.sanitize(inputA), inputSanitizer.sanitize(inputB));
-    String response = "%s is \"%s\" %s";
-    return String.format(response, inputA, conclusion, inputB);
+    String result = String.format(responseTemplate, inputA, conclusion, inputB);
+    log.info("Result: {}", result);
+    return result;
   }
 }
--- src/main/java/com/comparator/versionchecker/service/InputSanitizer.java
+++ src/main/java/com/comparator/versionchecker/service/InputSanitizer.java
@@ -1,6 +1,7 @@
 package com.comparator.versionchecker.service;
 
 import com.comparator.versionchecker.exception.NoAlphanumericInputException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import static com.comparator.versionchecker.util.Constants.ALPHANUMERIC_REGEX;
@@ -8,10 +9,13 @@ import static com.comparator.versionchecker.util.Constants.ALPHANUMERIC_REGEX;
 @Service
 public class InputSanitizer {
 
+    @Value("${error.message.no.alphanumeric.input}")
+    private String noAlphanumericMessage;
+
     public String sanitize(String userInput) {
         String inputSanitized = userInput.replaceAll(ALPHANUMERIC_REGEX, "");
         if (inputSanitized.isEmpty()) {
-            throw new NoAlphanumericInputException("Input must have at least 1 alphanumeric character.");
+            throw new NoAlphanumericInputException(noAlphanumericMessage);
         }
         return inputSanitized;
     }
